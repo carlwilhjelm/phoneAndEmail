39e7469e28d849bc99768086c09fa4fc463d6d5a
carlwilhjelm <carlwilhjelm@gmail.com>
2017-11-01 14:40:15 -0400

    Added 11/1

---
+++ phoneAndEmail.py
@@ -0,0 +1,46 @@
+#! python3
+
+import re, pyperclip
+
+#TODO: Create regex for phone
+phoneRegex = re.compile(r'''
+
+# 415-555-0000, 555-0000, (415) 555-0000, 555-0000 ext 12345, x12345
+
+(
+((\d\d\d) | (\(\d\d\d\)))?     # area code (optional)
+(\s|-)     # first separator
+\d\d\d     # first 3 digits
+-     # separator
+\d\d\d\d     # last 4 digits
+(((ext(\.)?\s)|x)(\d{2,5}))?     # extension (optional)
+)
+''', re.VERBOSE)
+
+#TODO: Create regex for email
+emailRegex = re.compile(r'''
+# some.+_thing@something.com
+
+[a-zA-Z0-9_.+]+    # name part
+@     # @ symbol
+[a-zA-Z0-9_.+]+     # domain
+
+''', re.VERBOSE)
+
+#TODO: Get the text off the clipboard
+text = pyperclip.paste()
+
+#TODO: Extract the em/ph from this text
+extractedPhone = phoneRegex.findall(text)
+extractedEmail = emailRegex.findall(text)
+
+allPhoneNumbers = []
+for phoneNumber in extractedPhone:
+    allPhoneNumbers.append(phoneNumber[0])
+
+#TODO: Copy the em/ph to clipboard
+
+results = '\n'.join(allPhoneNumbers) + '\n' + '\n'.join(extractedEmail)
+pyperclip.copy(results)
+
+
